103c3bc26297983bbb17f03bb364b2667593abda
victorL73 <47555715+victorL73@users.noreply.github.com>
2019-03-25 23:06:54 +0100

    Create essaie_code_capteur_DHT22.py

---
+++ essaie_code_capteur_DHT22.py
@@ -0,0 +1,17 @@
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT22
+
+
+pin = 21
+
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+
+	
+else:
+    print('Echec de lecture. Essayez encore!')
