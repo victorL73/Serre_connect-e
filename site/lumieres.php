aeb4f5d8c48950f0a382aa05df102f9f99a07d6a
victorL73 <47555715+victorL73@users.noreply.github.com>
2019-03-29 10:18:02 +0100

    Create lumieres.php

---
+++ site/lumieres.php
@@ -0,0 +1,14 @@
+<?php
+    system("gpio -g mode 4 out");
+    
+    if($_POST['executer'] == 'ON')
+    {
+        system("gpio -g write 4 0");
+    }
+    else
+    {
+        system("gpio -g write 4 1");
+    }
+    header('Location: index.php#pompes');
+    
+?>
