614d9919124dd216470e1919a1f364705d6a5641
victorL73 <47555715+victorL73@users.noreply.github.com>
2019-04-14 18:04:08 +0200

    Update pompe_on.py

--- relais/pompes/pompe_on.py
+++ relais/pompes/pompe_on.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
 import RPi.GPIO as GPIO ## Import GPIO library
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(7,False) ## Turn on GPIO pin 7
+GPIO.setup(18, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+GPIO.output(18,False) ## Turn on GPIO pin 7
